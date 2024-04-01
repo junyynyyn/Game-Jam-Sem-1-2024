75707bd77e812fc8d4f6c92046243e33df5ff5e7
Jun <junyynyyn@users.noreply.github.com>
2024-04-01 13:57:47 +1100

    Added basic player movement and main camera to scene

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var SPEED : float = 150.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (Vector2(input_dir.x, input_dir.y)).normalized()
+	
+	velocity = direction * SPEED
+	move_and_slide()
