c05e5312fbd5366b78114a97f801e0f1af4b4567
Jun <junyynyyn@users.noreply.github.com>
2024-04-02 17:36:22 +1100

    Added moneybags to scene

--- Scenes/Interactables/Locker.gd
+++ Scenes/Interactables/Locker.gd
@@ -3,9 +3,15 @@ class_name Locker extends Interactable
 @export var item: Item
 @export var locker_name: String
 
+var locked = true
+
 func use():
-	if (InventoryGlobal.has_item("paperclip")):
-		Globals.minigames.start_timing_game(item)
-		InventoryGlobal.remove_item("paperclip")
+	if (locked):
+		if (InventoryGlobal.has_item("paperclip")):
+			Globals.minigames.start_timing_game(item)
+			InventoryGlobal.remove_item("paperclip")
+			locked = false
+		else:
+			Globals.player.think(locker_name + " ... Locked")
 	else:
-		Globals.player.think(locker_name + " ... Locked")
+		Globals.player.think("Empty...")
