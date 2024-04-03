1b7f19ef7f2501fd249a06de700be9724baad2aa
Lok/Tom Chow <lohchness@gmail.com>
2024-04-03 15:41:10 +1100

    updated paperclip sprite, money bags, lockboxes

--- Scenes/Interactables/money_bag.gd
+++ Scenes/Interactables/money_bag.gd
@@ -1,4 +1,12 @@
 extends Interactable
 
+var sprites = [
+	"res://assets/asesprite/money_bag_1_128.png",
+	"res://assets/asesprite/money_bag_2_128.png"
+]
+
+func _ready():
+	$Sprite2D.texture = load(sprites.pick_random())
+
 func use():
 	print("Moneyy bag")
