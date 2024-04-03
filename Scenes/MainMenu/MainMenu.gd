f3f3fbde20920de7d42f3b6c5aa05823b3523aba
Jun <junyynyyn@users.noreply.github.com>
2024-04-03 16:55:16 +1100

    Added credit page

--- Scenes/MainMenu/MainMenu.gd
+++ Scenes/MainMenu/MainMenu.gd
@@ -3,13 +3,13 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_start_button_pressed():
+	MenuMusic.stop()
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_quit_button_pressed():
@@ -17,3 +17,6 @@ func _on_quit_button_pressed():
 
 func _on_info_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/MainMenu/InfoMenu.tscn")
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/MainMenu/Credits.tscn")
--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,3 +3,4 @@ extends Node2D
 func _ready():
 	Globals.main_scene = self
 	Globals.interactables = $Interactables
+	
